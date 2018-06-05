0c172d8abbc3e1f577ecf7adfcb9c6130a1d4ede
Chaouki Dhib <chaodhib@gmail.com>
2018-06-05 19:00:50 +0200

    fix css

--- viewCart.php
+++ viewCart.php
@@ -16,6 +16,7 @@ $cart = new Cart;
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="projet.css" media="all" type="text/css" /></head>
     <style>
     .container{padding: 50px;}
     input[type="number"]{width: 20%;}
